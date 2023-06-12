6d4fe6a3061ef195d367698260e394a2af32a496
viettdnde140214 <viettdnde140214@outlook.com>
2023-06-12 23:34:14 +0700

    add details page

--- WebShoppingMVC/Controllers/ProductController.cs
+++ WebShoppingMVC/Controllers/ProductController.cs
@@ -1,22 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebShoppingMVC.Data.Context;
 using WebShoppingMVC.Data.Repository.Interface;
 using WebShoppingMVC.Domain.Entity;
 using WebShoppingMVC.Domain.ModelList;
+using WebShoppingMVC.Services;
+using WebShoppingMVC.Services.Interface;
 
 namespace WebShoppingMVC.Controllers
 {
     public class ProductController : Controller
     {
-        private readonly IBaseRepository<Product> _baseRepository;
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IProductService _productService;
 
-        public ProductController(IBaseRepository<Product> baseRepository)
+        public ProductController(ApplicationDbContext dbContext, IProductService productService)
         {
-            _baseRepository = baseRepository;
+            _dbContext = dbContext;
+            _productService = productService;
         }
 
         public async Task<IActionResult> Details(int id)
         {
-            return View(await _baseRepository.GetById(id));
+            return View(await _productService.Get(id));
         }
     }
 }
